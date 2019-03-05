f442b1f2012f13c2f8893edb5012c14b9181d5c2
Marc BRUN <marc.brun@mines-paristech.fr>
2019-03-05 20:01:11 +0100

    Updates

---
+++ precache-manifest.b7c6f4116015f522defe6624d51f466c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "2bed69904f22342c89d6",
+    "url": "/blindtest-spotify/static/js/runtime~main.2bed6990.js"
+  },
+  {
+    "revision": "530a26b9535df72d73ee",
+    "url": "/blindtest-spotify/static/js/main.530a26b9.chunk.js"
+  },
+  {
+    "revision": "5d9107c347decab20cac",
+    "url": "/blindtest-spotify/static/js/2.5d9107c3.chunk.js"
+  },
+  {
+    "revision": "530a26b9535df72d73ee",
+    "url": "/blindtest-spotify/static/css/main.bbff6a99.chunk.css"
+  },
+  {
+    "revision": "e71cd31a75d1fe1902f866a136f9d7bd",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
